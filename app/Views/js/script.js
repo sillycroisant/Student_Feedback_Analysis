11488f21d565f2b44a9236381018f9f084b08f5d
HoangLe <hoangbq73@gmail.com>
2024-12-02 10:49:47 +0700

    Cap nhat giao dien lan 1

---
+++ app/Views/js/script.js
@@ -0,0 +1,7 @@
+function showSection(section) {
+    var sections = document.getElementsByClassName('content-section');
+    for (var i = 0; i < sections.length; i++) {
+        sections[i].classList.remove('active');
+    }
+    document.getElementById(section).classList.add('active');
+}
