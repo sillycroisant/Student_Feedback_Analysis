8833b6755c76383242f0a2bc50b03b1cced01d18
sillycroisant <huynzodai@gmail.com>
2024-12-04 01:21:14 +0700

    thay đổi 1 chút
    thay đổi tên, comment 1 số chỗ đoạn code để gọn hơn vs dễ hiểu, sửa 1 chút css cái footer

--- app/Views/js/script.js
+++ app/Views/js/script.js
@@ -1,3 +1,4 @@
+//hàm chuyển hướng người dùng đến trang "section" khác
 function showSection(section) {
     var sections = document.getElementsByClassName('content-section');
     for (var i = 0; i < sections.length; i++) {
